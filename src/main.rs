4c1875604f133ef7ea4d3a465bf4c78591ad12c0
Purbayan Chowdhury <pur.cho.99@gmail.com>
2025-04-11 11:54:01 +0530

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::io::{self, Write};
+
+fn check_token(ch: char) {
+    let mut token_map = HashMap::new();
+    token_map.insert('(', "LEFT_PAREN");
+    token_map.insert(')', "RIGHT_PAREN");
+    token_map.insert('{', "LEFT_BRACE");
+    token_map.insert('}', "RIGHT_BRACE");
+    token_map.insert(',', "COMMA");
+    token_map.insert(';', "SEMICOLON");
+    token_map.insert('+', "PLUS");
+    token_map.insert('-', "MINUS");
+    token_map.insert('*', "STAR");
+    token_map.insert('.', "DOT");
+
+    if let Some(token) = token_map.get(&ch) {
+        println!("{} {} null", token, ch);
+    } else {
+        println!("IDENTIFIER {} null", ch);
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -20,15 +41,9 @@ fn main() {
             });
 
             if !file_contents.is_empty() {
-                file_contents.chars().for_each(|c| match c {
-                    '(' => println!("LEFT_PAREN {} null", c),
-                    ')' => println!("RIGHT_PAREN {} null", c),
-                    '{' => println!("LEFT_BRACE {} null", c),
-                    '}' => println!("RIGHT_BRACE {} null", c),
-                    _ => (),
-                });
+                file_contents.chars().for_each(|c| check_token(c));
+                println!("EOF  null");
             }
-            println!("EOF  null");
         }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
